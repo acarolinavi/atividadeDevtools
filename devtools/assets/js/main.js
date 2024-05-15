6d2d93e086b451b8d27461ca232d4984fba8c46f
acarolinavi <145879241+acarolinavi@users.noreply.github.com>
2024-05-15 18:13:30 -0300

    devtools

---
+++ devtools/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   // TEXTO DIGITADO
+   var typed = new Typed(".linguagens", {
+     strings: ["BEM-VINDO(A)"],
+     typeSpeed: 100,
+     backSpeed: 60,
+     loop: true
+     });
+ });
+ 
